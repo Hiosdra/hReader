babcb181537194379a55cbbcd5b946a1c6488a26
Oskar Drozda <themppsplx@gmail.com>
2025-04-26 00:54:44 +0200

    refactor: remove unused imports from ArticleScreen for cleaner code

--- app/src/main/java/com/hiosdra/hreader/ui/article/ArticleScreen.kt
+++ app/src/main/java/com/hiosdra/hreader/ui/article/ArticleScreen.kt
@@ -34,7 +34,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -44,7 +43,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
