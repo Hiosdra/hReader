eabc2058244b03c93525e7080800fc74029bd069
Oskar Drozda <themppsplx@gmail.com>
2025-08-23 21:26:35 +0200

    Improve SettingsScreen layout by adding alignment support

--- app/src/main/java/com/hiosdra/hreader/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/hiosdra/hreader/ui/settings/SettingsScreen.kt
@@ -1,7 +1,6 @@
 package com.hiosdra.hreader.ui.settings
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -42,6 +41,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
