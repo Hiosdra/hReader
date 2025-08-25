f90b3c8e31401dd24b94c5230ed794174b0f060e
Oskar Drozda <themppsplx@gmail.com>
2025-08-25 16:00:21 +0200

    Remove dependency on versions plugin in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.android").version("2.2.10") apply false
     id("org.jetbrains.kotlin.plugin.compose").version("2.2.10") apply false
     id("androidx.room") version "2.7.2" apply false
-    id("com.github.ben-manes.versions") version "0.52.0" apply true
 }
 
 // Common versions used in multiple places
