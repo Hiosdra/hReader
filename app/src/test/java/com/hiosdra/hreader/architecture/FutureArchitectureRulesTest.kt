7fffb3db9b8dc1ef7cb5b955a4dee18e4eef27e4
Oskar Drozda <themppsplx@gmail.com>
2025-09-29 22:28:23 +0200

    Add architecture rules tests for UI and data dependencies

---
+++ app/src/test/java/com/hiosdra/hreader/architecture/FutureArchitectureRulesTest.kt
@@ -0,0 +1,62 @@
+package com.hiosdra.hreader.architecture
+
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.junit.ArchUnitRunner
+import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices
+import org.junit.Ignore
+import org.junit.runner.RunWith
+
+@Ignore
+@RunWith(ArchUnitRunner::class)
+@AnalyzeClasses(
+    packages = ["com.hiosdra.hreader"],
+    importOptions = [com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests::class]
+)
+class FutureArchitectureRulesTest {
+    @ArchTest
+    val uiShouldNotDependOnRemote: ArchRule = noClasses()
+        .that().resideInAPackage("com.hiosdra.hreader.ui..")
+        .should().dependOnClassesThat()
+        .resideInAnyPackage(
+            "com.hiosdra.hreader.data.remote.."
+        )
+
+    @ArchTest
+    val uiShouldNotDependOnEntities: ArchRule = noClasses()
+        .that().resideInAPackage("com.hiosdra.hreader.ui..")
+        .should().dependOnClassesThat()
+        .resideInAnyPackage("com.hiosdra.hreader.data.local.entity..")
+
+    @ArchTest
+    val onlyRepositoriesMayAccessDaos: ArchRule = noClasses()
+        .that().resideOutsideOfPackage("..repository..")
+        .should().dependOnClassesThat()
+        .resideInAnyPackage("com.hiosdra.hreader.data.local.dao..")
+
+    @ArchTest
+    val navigationShouldNotDependOnUi: ArchRule = noClasses()
+        .that().resideInAPackage("com.hiosdra.hreader.navigation..")
+        .should().dependOnClassesThat()
+        .resideInAnyPackage("com.hiosdra.hreader.ui..")
+
+    @ArchTest
+    val utilShouldNotDependOnData: ArchRule = noClasses()
+        .that().resideInAPackage("com.hiosdra.hreader.util..")
+        .should().dependOnClassesThat()
+        .resideInAnyPackage("com.hiosdra.hreader.data..")
+
+    @ArchTest
+    val repositoriesNaming: ArchRule = classes()
+        .that().resideInAPackage("..repository..")
+        .and().haveSimpleNameNotContaining("$")
+        .should().haveSimpleNameEndingWith("Repository")
+
+    @ArchTest
+    val topLevelPackagesNoCycles: ArchRule = slices()
+        .matching("com.hiosdra.hreader.(*)..")
+        .should().beFreeOfCycles()
+}
