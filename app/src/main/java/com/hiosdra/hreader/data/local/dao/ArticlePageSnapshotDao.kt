e04f4aa532462a74f2999fa26ad8961d88aa4f03
Oskar Drozda <themppsplx@gmail.com>
2026-08-01 16:00:27 +0200

    Harden offline article transitions

--- app/src/main/java/com/hiosdra/hreader/data/local/dao/ArticlePageSnapshotDao.kt
+++ app/src/main/java/com/hiosdra/hreader/data/local/dao/ArticlePageSnapshotDao.kt
@@ -26,7 +26,8 @@ interface ArticlePageSnapshotDao {
 
     @Query(
         "SELECT COUNT(*) FROM article_page_snapshots s INNER JOIN articles a " +
-            "ON a.id = CAST(s.entryId AS TEXT) WHERE s.isComplete = 1 AND " +
+            "ON a.id = CAST(s.entryId AS TEXT) AND s.originalUrl = a.url " +
+            "WHERE s.isComplete = 1 AND " +
             "((a.status IS NULL OR a.status != 'READ') OR " +
             "a.backlogFetchedAt IS NOT NULL OR a.starred = 1)"
     )
